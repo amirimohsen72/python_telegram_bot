83bcce1e79bd358dd780c618df6d1992cf19ba41
mohsen <amirimohsen72@gmail.com>
2025-07-18 23:23:02 +0330

    #4 main function run

--- core/bot.py
+++ core/bot.py
@@ -4,14 +4,21 @@ import os
 
 API_TOKEN = os.environ.get("API_TOKEN")
 
-# print(API_TOKEN)
-app = ApplicationBuilder().token(API_TOKEN).build()
-
 async def startproj(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await context.bot.send_message(chat_id=update.effective_chat.id,text=update.message)
+    # get data of message
+
+
+def main():
+    # print(API_TOKEN)
+    app = ApplicationBuilder().token(API_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", startproj))
+
+    app.run_polling()
 
 
 
-app.add_handler(CommandHandler("start", startproj))
-
-app.run_polling()
+if __name__ == "__main__":
+    main()
