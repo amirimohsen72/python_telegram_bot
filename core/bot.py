4e9ad2c20d194cad645955de85e7aa9c105deb1d
mohsen <amirimohsen72@gmail.com>
2025-07-18 23:59:19 +0330

    #6 هندل کردن کالکویری کلید شیشه ای و ریپلای کیبورد
    ReplyKeyboard

--- core/bot.py
+++ core/bot.py
@@ -1,8 +1,10 @@
-from telegram import Update , InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler,ContextTypes , CallbackQueryHandler
+from telegram import Update , InlineKeyboardButton, InlineKeyboardMarkup ,ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import ApplicationBuilder, CommandHandler,ContextTypes , CallbackQueryHandler, MessageHandler, filters
 import os
 
 API_TOKEN = os.environ.get("API_TOKEN")
+if not API_TOKEN:
+    raise ValueError("توکن ربات پیدا نشد! لطفاً متغیر API_TOKEN را تنظیم کنید.")
 
 async def startproj(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard= InlineKeyboardMarkup([
@@ -11,7 +13,25 @@ async def startproj(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         [InlineKeyboardButton('پروفایل' , callback_data='user_profile')],
     ])
 
-    await update.message.reply_text(f'سلام {update.effective_user.first_name} . خوش آمدی ', reply_markup=keyboard)
+    await update.message.reply_text( f"سلام {update.effective_user.first_name} 👋\nبه ربات خوش اومدی!\nیکی از گزینه‌ها رو انتخاب کن:", reply_markup=keyboard)
+
+async def handel_btn(update:Update, context:ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    
+    if query.data =='show_shortcuts':
+        keyboard = ReplyKeyboardMarkup(keyboard=[['/help', '/start'],["❌ حذف شورتکات"]], resize_keyboard=True,one_time_keyboard=False)
+        await query.message.reply_text('شورکات مورد نظر را انتخاب کنید' , reply_markup = keyboard) 
+
+    elif query.data == 'user_profile':
+        await query.message.reply_text("✏️ این بخش به‌زودی فعال می‌شود...")
+
+async def remove_shortcut_keyboard(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.text == "❌ حذف شورتکات":
+        await update.message.reply_text(
+            "✅ شورتکات‌ها حذف شدن.",
+            reply_markup=ReplyKeyboardRemove()
+        )
 
 
 def main():
@@ -19,6 +39,8 @@ def main():
     app = ApplicationBuilder().token(API_TOKEN).build()
 
     app.add_handler(CommandHandler("start", startproj))
+    app.add_handler(CallbackQueryHandler(handel_btn))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), remove_shortcut_keyboard))
 
     app.run_polling()
 
